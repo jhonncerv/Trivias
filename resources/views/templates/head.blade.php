098a6eaa882bb46b8c87a8ec662e03dcc4d42296
Fibonnacho <deluzmax@gmail.com>
2017-04-10 11:35:05 -0500

    favicon

--- resources/views/templates/head.blade.php
+++ resources/views/templates/head.blade.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
     <title>Twinings</title>
     <link rel='stylesheet' href='<?=main_css()?>' type='text/css' media='all' />
     <script type='text/javascript' src='https://code.jquery.com/jquery-1.12.4.min.js'></script>
