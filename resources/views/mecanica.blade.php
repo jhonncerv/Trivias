f54fc703f86599ba20e7c6aa5ecb4759114c5183
Jonathan <jonathan@thothnet.com>
2017-04-11 10:00:59 -0500

    estilizar un poco mecanica

--- resources/views/mecanica.blade.php
+++ resources/views/mecanica.blade.php
@@ -10,25 +10,30 @@
                 <span>Twinings</span> <span>Twinings #TéDeAltura</span>
             </div>
             <div class="tw-page__body">
-                <div class="container">    
-                <h1>Mecánica</h1>
-                <p><strong>Té de altura</strong></p>
-                <p>La dinámica se realizará a través de la página de Facebook (<a href="https://www.facebook.com/TwiningsMexico/" target="_blank">https://www.facebook.com/TwiningsMexico/</a>) de la siguiente manera:</p>
-                <ul>
-                    <li>Se publicará un video de anuncio de la dinámica, durante un período de 10 días naturales.</li>
-                    <li>El participante debe dar clic al post de la Ruta en Facebook.</li>
-                    <li>Llegarán a la página y podrán registrarse o ver las bases para conocer la dinámica. El registro será con su perfil de FB.</li>
-                    <li>Una vez terminado el registro, serán redireccionados a la página principal.</li>
-                    <li>Cada dos días, se publicará en Facebook 5 dinámicas relacionadas con la ruta del té.</li>
-                    <li>Cada vez que termine una dinámica, un cronómetro te avisará cuánto falta para que empiece la siguiente.</li>
-                    <li>No podrás continuar con la dinámica siguiente hasta que este tiempo termine.</li>
-                    <li>No podrás adelantar las dinámicas antes de que empiece cada una de ellas, sin embargo, podrás regresar al sitio cuando quieras y/o puedas para seguir adelante.</li>
-                    <li>La persona que sume más puntos, siguiendo las 5 dinámicas de los 5 países, participando en cada dinámica a lo largo de la campaña, será la ganadora.</li>
-                    <li>El resultado será inapelable.</li>
-                    <li>Entrega de premio. La persona que sume más puntos a lo largo de la campaña, será la ganadora. El ganador será notificado en el muro de <a href="https://www.facebook.com/TwiningsMexico/" target="_blank">https://www.facebook.com/TwiningsMexico/</a> y vía mensaje directo se le indicarán los detalles de la entrega del premio (el ganador y su acompañante deben contar con pasaporte y visa válidos).</li>
-                    <li>El premio consiste en un viaje redondo para dos personas por Aeroméxico en clase turista, el destino es Londres. Incluye 3 noches y 4 días de hospedaje (el hotel será definido por ACH Food México S. de R.L. de C.V., ya que depende de la disponibilidad de la fecha que el ganador decida), transportación de hotel a aeropuerto de ida y vuelta. Las fechas de vuelo están sujetas a disponibilidad de Aeroméxico y tienen vigencia hasta agosto de 2017. En caso de que el participante sea menor de edad, debe ir acompañado de un adulto.</li>
-                    <li>Si el participante que resulte ganador no responde a la notificación durante las siguientes 24 horas a la notificación de su victoria, vía mensaje directo, el premio pasará al siguiente participante que cumpla con las condiciones señaladas en la mecánica. Las condiciones y restricciones se deben leer y entender en: <a href="https://twinings.mx/tyco">https://twinings.mx/tyco</a>, y por el solo hecho de participar, se considerarán aceptadas por el participante. Los menores de edad requerirán de la autorización de sus padres o tutores. Participantes: Podrán participar personas que residan dentro de la República Mexicana, en caso de ser menor de edad, deben tener autorización de sus padres, tutores o custodios mediante la comprobación de identificación oficial.</li>
-                </ul>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-6 offset-lg-3 col-xs-12">
+                            <h1>Mecánica</h1>
+                            <p><strong>Té de altura</strong></p>
+                            <p>La dinámica se realizará a través de la página de Facebook (<a href="https://www.facebook.com/TwiningsMexico/" target="_blank">https://www.facebook.com/TwiningsMexico/</a>) de la siguiente manera:</p>
+                            <ul>
+                                <li>Se publicará un video de anuncio de la dinámica, durante un período de 10 días naturales.</li>
+                                <li>El participante debe dar clic al post de la Ruta en Facebook.</li>
+                                <li>Llegarán a la página y podrán registrarse o ver las bases para conocer la dinámica. El registro será con su perfil de FB.</li>
+                                <li>Una vez terminado el registro, serán redireccionados a la página principal.</li>
+                                <li>Cada dos días, se publicará en Facebook 5 dinámicas relacionadas con la ruta del té.</li>
+                                <li>Cada vez que termine una dinámica, un cronómetro te avisará cuánto falta para que empiece la siguiente.</li>
+                                <li>No podrás continuar con la dinámica siguiente hasta que este tiempo termine.</li>
+                                <li>No podrás adelantar las dinámicas antes de que empiece cada una de ellas, sin embargo, podrás regresar al sitio cuando quieras y/o puedas para seguir adelante.</li>
+                                <li>La persona que sume más puntos, siguiendo las 5 dinámicas de los 5 países, participando en cada dinámica a lo largo de la campaña, será la ganadora.</li>
+                                <li>El resultado será inapelable.</li>
+                                <li>Entrega de premio. La persona que sume más puntos a lo largo de la campaña, será la ganadora. El ganador será notificado en el muro de <a href="https://www.facebook.com/TwiningsMexico/" target="_blank">https://www.facebook.com/TwiningsMexico/</a> y vía mensaje directo se le indicarán los detalles de la entrega del premio (el ganador y su acompañante deben contar con pasaporte y visa válidos).</li>
+                                <li>El premio consiste en un viaje redondo para dos personas por Aeroméxico en clase turista, el destino es Londres. Incluye 3 noches y 4 días de hospedaje (el hotel será definido por ACH Food México S. de R.L. de C.V., ya que depende de la disponibilidad de la fecha que el ganador decida), transportación de hotel a aeropuerto de ida y vuelta. Las fechas de vuelo están sujetas a disponibilidad de Aeroméxico y tienen vigencia hasta agosto de 2017. En caso de que el participante sea menor de edad, debe ir acompañado de un adulto.</li>
+                                <li>Si el participante que resulte ganador no responde a la notificación durante las siguientes 24 horas a la notificación de su victoria, vía mensaje directo, el premio pasará al siguiente participante que cumpla con las condiciones señaladas en la mecánica. Las condiciones y restricciones se deben leer y entender en: <a href="https://twinings.mx/tyco">https://twinings.mx/tyco</a>, y por el solo hecho de participar, se considerarán aceptadas por el participante. Los menores de edad requerirán de la autorización de sus padres o tutores. Participantes: Podrán participar personas que residan dentro de la República Mexicana, en caso de ser menor de edad, deben tener autorización de sus padres, tutores o custodios mediante la comprobación de identificación oficial.</li>
+                            </ul>
+
+                        </div>
+                    </div>
                 </div>  
             </div>
             <a href="/" class="tw-form__button tw-page__return">Regresar</a>
