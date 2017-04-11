9390303a7c3862ba71d6473e6add333f78585cb4
Jonathan <jonathan@thothnet.com>
2017-04-11 01:40:44 -0500

    diponibilidad de dias

--- database/seeds/CiudadesTableSeeder.php
+++ database/seeds/CiudadesTableSeeder.php
@@ -13,13 +13,15 @@ class CiudadesTableSeeder extends Seeder
     public function run()
     {
         $cities = array('MARRUECOS', 'JAPON', 'INDIA', 'ARGENTINA', 'INGLATERRA');
-        $ayer = new Carbon('America/Mexico_City');
+        $hoy = new Carbon('America/Mexico_City');
 
+        $e = 0;
         for ($i = 0; $i < 5; $i++) {
             $ciudad = new \App\Ciudad();
             $ciudad->name = $cities[$i];
             $ciudad->available = 1;
-            $ciudad->publish = $ayer->subHours(6)->addDays($i);
+            $e += ($i > 1) ? 1 : 2;
+            $ciudad->publish = $hoy->subHours(6)->addDays($e);
             $ciudad->save();
         }
     }
