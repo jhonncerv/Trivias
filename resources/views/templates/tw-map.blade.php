9d2584d8624ad03b8b56e9f07622844078e03ba0
Fibonnacho <deluzmax@gmail.com>
2017-04-12 07:47:10 -0500

    mapa fix

--- resources/views/templates/tw-map.blade.php
+++ resources/views/templates/tw-map.blade.php
@@ -1,22 +1,33 @@
-<div class="tw-map">
+@if ($current = $ciudades->where('is_publish',1)[$ciudades->where('is_publish',1)->count()-1]->id) @endif
+<div class="tw-map tw-map--{{$current}}">
     <div class="tw-map__stage vertical-align--xs">
-        <div class="tw-map__stage__pins">
+        <div class="tw-map__pins tw-map__pins">
             <img src="<?=image_path('mapa.png')?>" alt="" />
-            <div class="tw-map__stage__pins__item tw-map__stage__pins__item--m" data-trigger-city="MARRUECOS"></div>
+            @foreach($ciudades->where('is_publish',1) as $ciudad)
+                 @if($ciudad->available == 0)
+                    @continue
+                @endif
+            <div class="tw-map__pins__item tw-map__pins__item--{{ $ciudad->id }}" data-trigger-city="{{ $ciudad->name }}"></div>
+            @endforeach
         </div>
     </div>
     <div class="tw-map__ctrls">
-        <div class="tw-map__ctrls__titles">
-            <div class="tw-map__ctrls__titles__item" data-trigger-city="MARRUECOS">
-                <span>01</span> <span>Marruecos</span>
-            </div>
-        </div>
-        <div class="tw-map__ctrls__dots">
+        <div class="tw-map__titles">
             @foreach($ciudades as $ciudad)
                 @if($ciudad->available == 0)
                     @continue
                 @endif
-            <div class="tw-map__ctrls__dots__item {{ $ciudad->is_publish ? 'tw-map__ctrls__dots__item--available':'' }}" data-trigger-city="{{ $ciudad->name }}"></div>
+            <div class="tw-map__titles__item tw-map__titles__item--{{ $ciudad->id }}" data-trigger-city="{{ $ciudad->name }}">
+                <span>0{{ $ciudad->id }}</span> <span>{{ $ciudad->name }}</span>
+            </div>
+            @endforeach
+        </div>
+        <div class="tw-map__dots">
+            @foreach($ciudades as $ciudad)
+                @if($ciudad->available == 0)
+                    @continue
+                @endif
+            <div class="tw-map__dots__item tw-map__dots__item--{{$ciudad->id}} {{ $ciudad->is_publish ? 'tw-map__dots__item--available':'' }}" data-id="{{$ciudad->id}}" {{ $ciudad->is_publish ? '':'data-trigger-city="'. $ciudad->name .'"' }}></div>
             @endforeach
         </div>
     </div>
