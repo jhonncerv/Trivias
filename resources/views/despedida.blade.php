66c08da28327cf0d19f2388ec0c187cbdc75285e
Fibonnacho <deluzmax@gmail.com>
2017-04-19 23:27:13 -0500

    Ending

--- resources/views/despedida.blade.php
+++ resources/views/despedida.blade.php
@@ -1,27 +1,24 @@
 <?php require_once "functions.php"; ?>
 @include('templates.head')
-<body class="page">
-    <main class="main" role="document">
-        <div class="tw-page">
-            <a href="/" class="tw-page__logo">
-                <img src="<?= image_path('twinings_of_london.png'); ?>" alt="Twinings of London" />
-            </a>
-            <div class="tw-page__title">
-                <span>Twinings</span> <span>Twinings #TéDeAltura</span>
-            </div>
-            <div class="tw-page__body">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-6 offset-lg-3 col-xs-12">
-
-
+    <body class="ending">
+        <main class="main" role="document">
+            <div class="vertical-align--xs">
+                <div class="ending-page">
+                    <img class="ending-page__map" src="<?=image_path('mapa.png')?>" alt="" />
+                    <div class="ending-page__wrapper">
+                        <img class="ending-page__logo" src="<?= image_path('twinings_of_london.png'); ?>" alt="Twinings of London" />
+                        <img class="ending-page__pin" src="<?=image_path('pin.png')?>" alt="" />
+                        <div class="ending-page__title">Gracias por participar</div>
+                        <p>Pronto anunciaremos a los ganadores.<br />Sigue todos los detalles en nuestras redes sociales.</p>
+                        <div class="ending-page__links">
+                            <a href="https://www.facebook.com/TwiningsMexico" target="_blank"><i class="fa fa-facebook"></i></a>
+                            <a href="https://twitter.com/twiningsmexico" target="_blank"><i class="fa fa-twitter"></i></a>
                         </div>
+                        <p><a href="https://twitter.com/twiningsmexico" target="_blank">@TwiningsMexico</a></p>
                     </div>
-                </div>  
+                </div>
             </div>
-            <a href="/" class="tw-form__button tw-page__return">Regresar</a>
-        </div>
-    </main>
-    <script type='text/javascript' src='<?=main_js()?>'></script>
-</body>
+        </main>
+        <script type='text/javascript' src='<?=main_js()?>'></script>
+    </body>
 </html>
